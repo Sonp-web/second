b178608f48c189fa404fc8cb6bb45e156280ae6f
Egor <pattrixd@yandex.by>
2025-05-22 23:45:36 +0300

    Creating tabs, without js

--- src/js/main.js
+++ src/js/main.js
@@ -19,7 +19,27 @@ close.addEventListener("click", () => {
   menu.classList.remove("header__menu_active");
   document.body.style.overflow = "";
 });
+/*try {
+  const tabs = document.querySelectorAll(".catalog__tab");
+  const contents = document.querySelectorAll(".catalog__content-item");
 
+  tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+      // Удаляем активный класс у всех табов и контента
+      tabs.forEach((t) => t.classList.remove("catalog__tab_active"));
+      contents.forEach((c) => (c.style.display = "none"));
+
+      // Добавляем активный класс к нажатому табу и показываем соответствующий контент
+      tab.classList.add("catalog__tab_active");
+      contents[index].style.display = "block";
+    });
+  });
+
+  // Показываем первый контент при загрузке
+  contents.forEach((c, i) => (c.style.display = i === 0 ? "block" : "none"));
+} catch (e) {}*/
+
+// Обратите внимание, что значение block (в двух местах) можно спокойно поменять на flex, если вам это необходимо
 try {
   const swiper = new Swiper(".works__swiper", {
     slidesPerView: 1,
